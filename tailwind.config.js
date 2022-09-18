b84cef39a1ad0ebd7708b20faea588914df8315a
juanpre <prendas52@gmail.com>
2022-09-18 01:47:15 -0600

    Tailwind config theme and nav created

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "weather-primary": "#2290CD",
+        "weather-secondary": "#219CD5",
+
+      }
+    },
+    fontFamily: {
+      Roboto: ["Roboto, sans-serif"]
+    },
+    container: {
+      padding: "2rem",
+      center: true,
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+    },
   },
   plugins: [],
 }
